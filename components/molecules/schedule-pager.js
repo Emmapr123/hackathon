bcb4b0485b5ff926658cd4c109975f4d601792f3
Emmapr123 <emmapriester@hotmail.com>
2022-03-29 15:18:50 +0100

    Weekly and monthly insights

--- components/molecules/schedule-pager.js
+++ components/molecules/schedule-pager.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Layout, Text, ViewPager } from '@ui-kitten/components';
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { Layout, ViewPager } from "@ui-kitten/components";
+import { UsageTimeContainer } from "../organisms/usage-time-container";
 
-const ViewPagerSimple = ({selectedIndex, setSelectedIndex}) => {
+const ViewPagerSimple = ({ selectedIndex, setSelectedIndex }) => {
   return (
     <ViewPager
       selectedIndex={selectedIndex}
-      onSelect={index => setSelectedIndex(index)}>
-      <Layout
-        style={styles.tab}
-        level='2'>
-        <Text category='h5'>Daily</Text>
+      onSelect={(index) => setSelectedIndex(index)}
+    >
+      <Layout style={styles.tab} level="2">
+        <ScrollView directionalLockEnabled={true}>
+          <UsageTimeContainer time={"Mon"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Tue"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Wed"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Thu"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Fri"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Sat"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Sun"} percentage={35} amount={3.5} />
+        </ScrollView>
       </Layout>
-      <Layout
-        style={styles.tab}
-        level='2'>
-        <Text category='h5'>Monthly</Text>
-      </Layout>
-      <Layout
-        style={styles.tab}
-        level='2'>
-        <Text category='h5'>Yearly</Text>
+      <Layout style={styles.tab} level="2">
+        <ScrollView>
+          <UsageTimeContainer time={"Jan"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Feb"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Mar"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Apr"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"May"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Jun"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Jul"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Aug"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Sep"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Oct"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Nov"} percentage={35} amount={3.5} />
+          <UsageTimeContainer time={"Dec"} percentage={35} amount={3.5} />
+        </ScrollView>
       </Layout>
     </ViewPager>
   );
@@ -28,9 +42,10 @@ const ViewPagerSimple = ({selectedIndex, setSelectedIndex}) => {
 
 const styles = StyleSheet.create({
   tab: {
-    height: 400,
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 200,
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
--- components/screens/insight-screen.js
+++ components/screens/insight-screen.js
@@ -1,18 +1,26 @@
 import { StyleSheet, View } from "react-native";
-import { Text, Layout } from '@ui-kitten/components';
-import TabBarSimple from './../molecules/top-nav';
-import React from 'react';
-import ViewPagerSimple from './../molecules/schedule-pager.js'
+import { Text } from "@ui-kitten/components";
+import TabBarSimple from "./../molecules/top-nav";
+import React from "react";
+import ViewPagerSimple from "./../molecules/schedule-pager.js";
 
 const InsightScreen = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-    return(
-        <View>
-            <TabBarSimple selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}  />
-            <ViewPagerSimple selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
-        </View>
-    )
+  return (
+    <View>
+      <Text>Hello world</Text>
+      <TabBarSimple
+        selectedIndex={selectedIndex}
+        setSelectedIndex={setSelectedIndex}
+      />
+      <ViewPagerSimple
+        selectedIndex={selectedIndex}
+        setSelectedIndex={setSelectedIndex}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   title: {
