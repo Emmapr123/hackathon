bc8245b51a6b14ca2ee428241218e9403fee278a
andrew-brook-rad <andrew.brook@ovoenergy.com>
2022-03-29 14:37:54 +0100

    merge with main

--- components/screens/insight-screen.js
+++ components/screens/insight-screen.js
@@ -1,5 +1,4 @@
 import { StyleSheet, View } from "react-native";
-<<<<<<< HEAD
 import { Text, Layout } from '@ui-kitten/components';
 import TabBarSimple from './../molecules/top-nav';
 import React from 'react';
@@ -14,20 +13,6 @@ const InsightScreen = () => {
             <ViewPagerSimple selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
         </View>
     )
-=======
-import { UsageTimeContainer } from "../organisms/usage-time-container";
-
-const InsightScreen = () => {
-  return (
-    <View>
-      <UsageTimeContainer time={'Jan'} percentage={75} amount={100}/>
-      <UsageTimeContainer time={'Feb'} percentage={35} amount={100}/>
-      <UsageTimeContainer time={'Mar'} percentage={50} amount={100}/>
-    </View>
-
-  );
->>>>>>> 4d3c51ba6ad66cdd4b48e681e4ada4bb93086d70
-};
 
 const styles = StyleSheet.create({
   title: {
