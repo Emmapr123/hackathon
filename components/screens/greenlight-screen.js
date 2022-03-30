201f2499cb9792ab5d29780cdd0eb08ebeb5e31c
Emmapr123 <emmapriester@hotmail.com>
2022-03-30 12:22:56 +0100

    Merge

--- components/screens/greenlight-screen.js
+++ components/screens/greenlight-screen.js
@@ -3,12 +3,10 @@ import { Text } from "@ui-kitten/components";
 import React from "react";
 import InsightItemList from "./../molecules/insight-card";
 
-const GreenLightScreen = () => {
+const GreenLightScreen = ({insightData}) => {
   const shower = new Array(1).fill({
     title: "Shower",
     subTitle: "This is the energy used to power your shower with hot water",
-    tipTitle: "Go with the flow",
-    tipBody: 'Start saving money and energy while taking a shower: install a water-efficient showerhead or a shower flow reducer. The best bit? These are usually free  check with your water company.'
   });
 
   const heating = new Array(1).fill({
